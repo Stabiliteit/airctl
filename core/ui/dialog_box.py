090b0f9850b3d40a4464d3cf15401768a369e8b6
Anish Roy <aroy262692@gmail.com>
2026-01-29 15:29:52 +0530

    update(DialogBox): size of the box

--- core/ui/dialog_box.py
+++ core/ui/dialog_box.py
@@ -17,15 +17,15 @@ class DialogBox:
             title=f"Connect to {self.ssid}",
             modal=True,
             transient_for=self.parent,
-            default_width=300,
-            default_height=150,
+            default_width=400,
+            default_height=200,
         )
 
-        self.content_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-        self.content_box.set_margin_top(15)
-        self.content_box.set_margin_bottom(15)
-        self.content_box.set_margin_start(15)
-        self.content_box.set_margin_end(15)
+        self.content_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=16)
+        self.content_box.set_margin_top(24)
+        self.content_box.set_margin_bottom(24)
+        self.content_box.set_margin_start(24)
+        self.content_box.set_margin_end(24)
 
         self.dialog_window.set_child(self.content_box)
 
@@ -34,15 +34,37 @@ class DialogBox:
             return
         self._shown = True
 
-        label = Gtk.Label(label=f"Enter password for {self.ssid}")
-        label.set_halign(Gtk.Align.START)
+        header_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
+
+        title_label = Gtk.Label(label=f"Connect to {self.ssid}")
+        title_label.set_halign(Gtk.Align.START)
+        title_label.add_css_class("title-3")
+
+        subtitle_label = Gtk.Label(label="Enter the network password")
+        subtitle_label.set_halign(Gtk.Align.START)
+        subtitle_label.add_css_class("dim-label")
+        subtitle_label.set_opacity(0.7)
+
+        header_box.append(title_label)
+        header_box.append(subtitle_label)
+
+        entry_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
+        entry_box.set_margin_top(8)
+
+        entry_label = Gtk.Label(label="Password")
+        entry_label.set_halign(Gtk.Align.START)
+        entry_label.add_css_class("caption-heading")
 
         entry = Gtk.PasswordEntry()
         entry.set_show_peek_icon(True)
+        entry.add_css_class("password-entry")
 
-        buttons = Gtk.Box(spacing=10)
+        entry_box.append(entry_label)
+        entry_box.append(entry)
+
+        buttons = Gtk.Box(spacing=12)
         buttons.set_halign(Gtk.Align.END)
-        buttons.set_margin_top(10)
+        buttons.set_margin_top(16)
 
         finished = False
 
@@ -55,9 +77,11 @@ class DialogBox:
             self.callback(value)
 
         cancel = Gtk.Button(label="Cancel")
+        cancel.set_size_request(100, -1)
         cancel.connect("clicked", lambda *_: finish(None))
 
         connect = Gtk.Button(label="Connect")
+        connect.set_size_request(100, -1)
         connect.add_css_class("suggested-action")
         connect.connect("clicked", lambda *_: finish(entry.get_text()))
 
@@ -71,9 +95,15 @@ class DialogBox:
         buttons.append(cancel)
         buttons.append(connect)
 
-        self.content_box.append(label)
-        self.content_box.append(entry)
+        self.content_box.append(header_box)
+        self.content_box.append(entry_box)
         self.content_box.append(buttons)
 
         self.dialog_window.present()
         entry.grab_focus()
+
+    def confirmation(self):
+        pass
+
+    def error(self):
+        pass
