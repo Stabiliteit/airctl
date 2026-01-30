54d645919c6818cfe846653b92e1bf5375cc4fdf
Anish Roy <aroy262692@gmail.com>
2026-01-31 00:52:23 +0530

    fix: styles import

--- airctl/main.py
+++ airctl/main.py
@@ -12,7 +12,7 @@ from airctl.ui.wifi_off_widget import WiFiOffWidget
 
 
 css_provider = Gtk.CssProvider()
-css_provider.load_from_path("styles/style.css")
+css_provider.load_from_path("airctl/styles/style.css")
 Gtk.StyleContext.add_provider_for_display(
     Gdk.Display.get_default(), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
 )
